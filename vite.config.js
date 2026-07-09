5b8370b750b21631c5659ffeb2ff552a3dc5c19c
kiruthika0225 <kiruthikasaravanan08@gmail.com>
2026-07-09 20:34:55 +0530

    Fix GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio/index.html',
   plugins: [react()],
-  // ... rest of file
+  base: '/Portfolio/',
 })
