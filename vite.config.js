02dd8055c9ea5ce35c850cb3593c8a771ac89881
kiruthika0225 <kiruthikasaravanan08@gmail.com>
2026-07-09 12:22:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/Portfolio/index.html',
   plugins: [react()],
   // ... rest of file
 })
